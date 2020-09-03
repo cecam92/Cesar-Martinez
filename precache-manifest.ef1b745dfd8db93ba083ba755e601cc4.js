67e1d9e96cc430f6de36335d408cee7cc7f55005
Cesar Martinez <cecam92@hotmail.com>
2020-09-04 01:23:53 +1000

    Updates

---
+++ precache-manifest.ef1b745dfd8db93ba083ba755e601cc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b73c4eecc878ac46c37dd53d162570",
+    "url": "/Links/index.html"
+  },
+  {
+    "revision": "8c9741a446f85604385a",
+    "url": "/Links/static/css/main.9a4a5804.chunk.css"
+  },
+  {
+    "revision": "f2eec27dc6844e67f2ea",
+    "url": "/Links/static/js/2.7466236b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Links/static/js/2.7466236b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c9741a446f85604385a",
+    "url": "/Links/static/js/main.1e72f64e.chunk.js"
+  },
+  {
+    "revision": "15cb9a3110dada058755",
+    "url": "/Links/static/js/runtime-main.6ed811bc.js"
+  }
+]);
