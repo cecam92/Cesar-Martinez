438fbbc1db6339ef885551ef9fb2a5dfc046282b
Cesar Martinez <cecam92@hotmail.com>
2020-09-08 23:08:12 +1000

    mobile design done

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -2,14 +2,60 @@ import React from "react";
 import AboutMe from "../AboutMe";
 import Portfolio from "../Portfolio";
 import "./styles.scss";
+import JS from "../../assets/js.png";
+import HTML from "../../assets/html.png";
+import CSS from "../../assets/css.png";
+import ReactIcon from "../../assets/react.png";
+import Rick from "../../assets/pickle.png";
+import graph from "../../assets/graphql.png";
+import bootstrap from "../../assets/bootstrap.png";
+import galeria from "../../assets/galeria.png";
 
 const Main = () => {
+  const projects = [
+    {
+      title: "Rick and Morty",
+      description: ` I am a Network Engineer and highly skilled Frontend Developer on
+      Single Page Applications with React JS. I have knowledge and good
+      practices of Hooks, Redux, Router, SASS, RESTful and GraphQL APIs.
+      I'm a problem solver, strong team player, quick learner and
+      adaptable person, who delivers reliable results with excellent
+      project management skills.`,
+      techs: [
+        { name: "html", icon: HTML },
+        { name: "CSS", icon: CSS },
+        { name: "JS", icon: JS },
+        { name: "React", icon: ReactIcon },
+        { name: "graphQL", icon: graph },
+      ],
+      picture: Rick,
+      url: "https://cecam92.github.io/RickandMorty/",
+      repository: "https://github.com/cecam92/RickandMorty",
+    },
+    {
+      title: "Layout Galeria",
+      description: ` I am a Network Engineer and highly skilled Frontend Developer on
+      Single Page Applications with React JS. I have knowledge and good
+      practices of Hooks, Redux, Router, SASS, RESTful and GraphQL APIs.
+      I'm a problem solver, strong team player, quick learner and
+      adaptable person, who delivers reliable results with excellent
+      project management skills.`,
+      techs: [
+        { name: "html", icon: HTML },
+        { name: "CSS", icon: CSS },
+        { name: "JS", icon: JS },
+        { name: "React", icon: ReactIcon },
+        { name: "bootstrap", icon: bootstrap },
+      ],
+      picture: galeria,
+      url: "https://cecam92.github.io/Galeria/",
+      repository: "https://github.com/cecam92/Galeria",
+    },
+  ];
   return (
     <main>
       <AboutMe />
-      <Portfolio />
-      <AboutMe />
-      <AboutMe />
+      <Portfolio projects={projects} />
     </main>
   );
 };
--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -2,106 +2,68 @@ import React, { Fragment } from "react";
 import Button from "react-bootstrap/button";
 import "./styles.scss";
 
-import JS from "../../assets/js.png";
-import HTML from "../../assets/html.png";
-import CSS from "../../assets/css.png";
-import ReactIcon from "../../assets/react.png";
-import Rick from "../../assets/pickle.png";
-import graph from "../../assets/graphql.png";
-import bootstrap from "../../assets/bootstrap.png";
-
-const Porfolio = () => {
-  const projects = [
-    {
-      title: "Layout Galeria",
-      description: ` I am a Network Engineer and highly skilled Frontend Developer on
-      Single Page Applications with React JS. I have knowledge and good
-      practices of Hooks, Redux, Router, SASS, RESTful and GraphQL APIs.
-      I'm a problem solver, strong team player, quick learner and
-      adaptable person, who delivers reliable results with excellent
-      project management skills.`,
-      technologies: [{ HTML }, { CSS }, { ReactIcon }, { JS }, { bootstrap }],
-      picture: { Rick },
-    },
-    {
-      title: "Rick and Morty",
-      description: ` I am a Network Engineer and highly skilled Frontend Developer on
-      Single Page Applications with React JS. I have knowledge and good
-      practices of Hooks, Redux, Router, SASS, RESTful and GraphQL APIs.
-      I'm a problem solver, strong team player, quick learner and
-      adaptable person, who delivers reliable results with excellent
-      project management skills.`,
-      technologies: [{ HTML }, { CSS }, { ReactIcon }, { JS }, { graph }],
-      picture: { Rick },
-    },
-  ];
+const Porfolio = (props) => {
+  const { projects } = props;
 
   return (
     <Fragment>
-      {projects.map((project) => {})}
-      <section id="portfolio" className=" light">
+      <section className="projects ligth">
         <h2 className="section-title">Portfolio</h2>
         <div className="text_container">
           <p>Portfolio's description</p>
         </div>
-        <figure className="portfolio-image">
-          <img src={Rick} alt="Portfolio" />
-        </figure>
-        <div className="text_container">
-          <p>
-            I am a Network Engineer and highly skilled Frontend Developer on
-            Single Page Applications with React JS. I have knowledge and good
-            practices of Hooks, Redux, Router, SASS, RESTful and GraphQL APIs.
-            I'm a problem solver, strong team player, quick learner and
-            adaptable person, who delivers reliable results with excellent
-            project management skills.
-          </p>
-        </div>
-        <div className="section-buttons">
-          <div className="buttons-container">
-            <Button className="btn btn-primary">
-              <a
-                href="https://cecam92.github.io/RickandMorty/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Demo
-              </a>
-            </Button>
-            <Button className="btn btn-primary">
-              <a
-                href="https://github.com/cecam92/RickandMorty"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Github
-              </a>
-            </Button>
-          </div>
-        </div>
-        <div className="icon-container">
-          <i>
-            <img className="icon" src={HTML} alt="html icon" />
-            <h6 className="icon-caption">html</h6>
-          </i>
-          <i>
-            <img className="icon" src={CSS} alt="css icon" />
-            <h6 className="icon-caption">CSS</h6>
-          </i>
-          <i>
-            <img className="icon" src={JS} alt="Javascript icon" />
-            <h6 className="icon-caption">JS</h6>
-          </i>
-          <i>
-            <img className="icon" src={ReactIcon} alt="React icon" />
-            <h6 className="icon-caption">React</h6>
-          </i>
-
-          <i>
-            <img className="icon" src={graph} alt="GraphQL icon" />
-            <h6 className="icon-caption">GraphQL</h6>
-          </i>
-        </div>
+        {projects.map((project, index) => {
+          return (
+            <article className="projects " key={index}>
+              <h2 className="project-title">{project.title}</h2>
+              <figure className="portfolio-image">
+                <img src={project.picture} alt="Portfolio" />
+              </figure>
+              <div className="text_container">
+                <p>
+                  I am a Network Engineer and highly skilled Frontend Developer
+                  on Single Page Applications with React JS. I have knowledge
+                  and good practices of Hooks, Redux, Router, SASS, RESTful and
+                  GraphQL APIs. I'm a problem solver, strong team player, quick
+                  learner and adaptable person, who delivers reliable results
+                  with excellent project management skills.
+                </p>
+              </div>
+              <div className="section-buttons">
+                <div className="buttons-container">
+                  <Button className="btn btn-primary">
+                    <a
+                      href={project.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Demo
+                    </a>
+                  </Button>
+                  <Button className="btn btn-primary">
+                    <a
+                      href={project.repository}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Github
+                    </a>
+                  </Button>
+                </div>
+              </div>
+              <div className="icon-container">
+                {project.techs.map((tech, index) => {
+                  return (
+                    <i key={index}>
+                      <img className="icon" src={tech.icon} alt="html icon" />
+                      <h6 className="icon-caption">{tech.name}</h6>
+                    </i>
+                  );
+                })}
+              </div>
+            </article>
+          );
+        })}
       </section>
     </Fragment>
   );
