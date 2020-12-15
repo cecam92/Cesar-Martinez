91a92d01f9a8a26e3bc30eaacd7efa37e7305c3a
Cesar Martinez <cecam92@hotmail.com>
2020-12-15 23:24:12 +1100

    petgram project added and Galeria deleted

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,103 +1,98 @@
-import React from "react";
-import AboutMe from "../AboutMe";
-import Portfolio from "../Portfolio";
-import JS from "../../assets/js.png";
-import HTML from "../../assets/html.png";
-import Redux from "../../assets/redux.png";
-import CSS from "../../assets/css.png";
-import ReactIcon from "../../assets/react.png";
-import Sass from "../../assets/sass.png";
-import Rest from "../../assets/rest.png";
-import Rick from "../../assets/pickle.png";
-import RickLaptop from "../../assets/RickLaptop.png";
+import React from 'react'
+import AboutMe from '../AboutMe'
+import Portfolio from '../Portfolio'
+import JS from '../../assets/js.png'
+import Redux from '../../assets/redux.png'
+import CSS from '../../assets/css.png'
+import ReactIcon from '../../assets/react.png'
+import Sass from '../../assets/sass.png'
+import Rest from '../../assets/rest.png'
+import Rick from '../../assets/pickle.png'
+import RickLaptop from '../../assets/RickLaptop.png'
 
-import graph from "../../assets/graphql.png";
-import bootstrap from "../../assets/bootstrap.png";
-import galeria from "../../assets/galeria.png";
-import GaleriaLg from "../../assets/galeriaLg.png";
-import Crypto from "../../assets/crypto.png";
-
-import Technologies from "../Technologies";
-import Footer from "../Footer";
+import graph from '../../assets/graphql.png'
+import StyledComponents from '../../assets/styledComponents.png'
+import Crypto from '../../assets/crypto.png'
+import Petgram from '../../assets/petgram.png'
+import Technologies from '../Technologies'
+import Footer from '../Footer'
 
 const Main = () => {
   const projects = [
     {
-      title: "Rick and Morty",
+      title: 'Rick and Morty',
       description: [
         {
           paragraph: ` I decided to create this website where you can find every character from the TV show Rick and Morty. 
-          All the data presented in this project is from the open source Rick and Morty API.`,
+          All the data presented in this project is from the open source Rick and Morty API.`
         },
         {
           paragraph: `  I realized an infinite scroll single page application on React JS. 
           The SPA fetches data from the RESTful API and manages the requested data with Redux. 
           Using React Router allows me to redirect the user to any character by typing its ID, and from the GraphQL API I 
-          request the information of the selected character.`,
-        },
+          request the information of the selected character.`
+        }
       ],
 
       techs: [
-        { name: "React", icon: ReactIcon },
-        { name: "GraphQL", icon: graph },
-        { name: "REST", icon: Rest },
-        { name: "Redux", icon: Redux },
-        { name: "Sass", icon: Sass },
+        { name: 'React', icon: ReactIcon },
+        { name: 'GraphQL', icon: graph },
+        { name: 'REST', icon: Rest },
+        { name: 'Redux', icon: Redux },
+        { name: 'Sass', icon: Sass }
       ],
       pictureSm: Rick,
       pictureLg: RickLaptop,
-      url: "https://cecam92.github.io/RickandMorty/",
-      repository: "https://github.com/cecam92/RickandMorty",
+      url: 'https://cecam92.github.io/RickandMorty/',
+      repository: 'https://github.com/cecam92/RickandMorty'
     },
     {
-      title: "Galeria",
+      title: 'Petgram',
       description: [
         {
-          paragraph: `Galeria is a non-functional SPA. The intention of this project is to create an eCommerce mock-up with a CSS framework 
-          such as Bootstrap, and a variety of React Components.`,
+          paragraph: `Petgram is a pet version of Instagram. Here you can find pets per category and save their pictures to your favorites
+          if you sign in.`
         },
         {
-          paragraph: `This mock-up has the most common components in a website, like forms, breadcrumbs, carousel, 
-          portals and a dropdown navigation bar. The framework of the online store is ready-made for customisation and the 
-          clean-cut style of the site simplifies the user’s experience. `,
-        },
+          paragraph: `This project was styled with Styled Components, request data with GraphQL from a local API and uses React Context to manage the state of the session storage, in other words, maintain and checked if the user has log in and enable the favorite section. 
+           `
+        }
       ],
       techs: [
-        { name: "Html", icon: HTML },
-        { name: "CSS", icon: CSS },
-        { name: "JavaScript", icon: JS },
-        { name: "React", icon: ReactIcon },
-        { name: "Bootstrap", icon: bootstrap },
+        { name: 'React', icon: ReactIcon },
+        { name: 'Styled Components', icon: StyledComponents },
+        { name: 'GraphQL', icon: graph },
+        { name: 'JavaScript', icon: JS }
       ],
-      pictureSm: galeria,
-      pictureLg: GaleriaLg,
-      url: "https://cecam92.github.io/Galeria/",
-      repository: "https://github.com/cecam92/Galeria",
+      pictureSm: Petgram,
+      pictureLg: Petgram,
+      url: 'http://cecam92-petgram.netlify.app/',
+      repository: 'https://github.com/cecam92/petgram'
     },
     {
-      title: "CryptoTracker",
+      title: 'CryptoTracker',
       description: [
         {
           paragraph: `CryptoTracker is an IOS and Android mobile application developed in React Native.
-          This app tracks the price of crypto coins in the market.`,
+          This app tracks the price of crypto coins in the market.`
         },
         {
           paragraph: `CryptoTracker shows the behaviour of currencies over the last hour, 
           their price in USD and their value relative to other coins.
             This data is gathered from an open source RESTful API.
-          `,
-        },
+          `
+        }
       ],
       techs: [
-        { name: "React Native", icon: ReactIcon },
-        { name: "REST", icon: Rest },
-        { name: "CSS", icon: CSS },
-        { name: "JavaScript", icon: JS },
+        { name: 'React Native', icon: ReactIcon },
+        { name: 'REST', icon: Rest },
+        { name: 'CSS', icon: CSS },
+        { name: 'JavaScript', icon: JS }
       ],
       pictureSm: Crypto,
-      repository: "https://github.com/cecam92/CryptoTracker",
-    },
-  ];
+      repository: 'https://github.com/cecam92/CryptoTracker'
+    }
+  ]
   return (
     <main>
       <AboutMe />
@@ -105,7 +100,7 @@ const Main = () => {
       <Technologies />
       <Footer />
     </main>
-  );
-};
+  )
+}
 
-export default Main;
+export default Main
