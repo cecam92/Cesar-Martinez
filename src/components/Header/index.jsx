88870eeab5c027b292da3be4418095d9e5a94b2d
Cesar Martinez <cecam92@hotmail.com>
2021-02-06 22:04:26 +1100

    cv updated

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,30 +1,30 @@
-import React, { Fragment } from "react";
-import { useSpring, animated as a } from "react-spring";
+import React, { Fragment } from 'react'
+import { useSpring, animated as a } from 'react-spring'
 
-import "./styles.scss";
+import './styles.scss'
 
 const Header = (props) => {
-  const { data } = props;
+  const { data } = props
   const headerProps = useSpring({
     from: {
-      transform: "scale(1)",
-      opacity: 1,
+      transform: 'scale(1)',
+      opacity: 1
     },
     opacity: 1 - window.scrollY / 100,
-    transform: `scale(${data})`,
-  });
+    transform: `scale(${data})`
+  })
   return (
     <Fragment>
-      <header id="home">
+      <header id='home'>
         <a.div style={headerProps}>
-          <h1 className="scroll-sizing">Cesar Martinez</h1>
-          <h2 className="scroll-sizing" id="profession">
-            Network Engineer | Front End Developer
+          <h1 className='scroll-sizing'>Cesar Martinez</h1>
+          <h2 className='scroll-sizing' id='profession'>
+            Frontend Developer | Network Engineer
           </h2>
         </a.div>
       </header>
     </Fragment>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
