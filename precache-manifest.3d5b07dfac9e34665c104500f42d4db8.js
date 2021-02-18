2ec1f942fb9ee2fb624993ed4b45ff0e946c3f90
Cesar Martinez <cecam92@hotmail.com>
2021-02-18 22:21:47 +1100

    Updates

---
+++ precache-manifest.3d5b07dfac9e34665c104500f42d4db8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63cd27f40befe71e6b8415dd3411a55e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04503eb632a02b9de7c4",
+    "url": "/static/css/main.1b61fd87.chunk.css"
+  },
+  {
+    "revision": "5da5e835a91290310c43",
+    "url": "/static/js/2.18017314.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.18017314.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04503eb632a02b9de7c4",
+    "url": "/static/js/main.82c94307.chunk.js"
+  },
+  {
+    "revision": "992a49b91a9776c3dc5a",
+    "url": "/static/js/runtime-main.11a7be33.js"
+  },
+  {
+    "revision": "8768f01b27a4c39f5f8837bccc683ecc",
+    "url": "/static/media/RickLaptop.8768f01b.png"
+  },
+  {
+    "revision": "da00c0833396eda8c79dfd38a8b76940",
+    "url": "/static/media/crypto.da00c083.png"
+  },
+  {
+    "revision": "87f81329961ea31073919268761f4848",
+    "url": "/static/media/cv.87f81329.pdf"
+  },
+  {
+    "revision": "687624a6efd2748af9b95a205a098676",
+    "url": "/static/media/graphql.687624a6.png"
+  },
+  {
+    "revision": "452587d6b37e313f404e482333938ff7",
+    "url": "/static/media/js.452587d6.png"
+  },
+  {
+    "revision": "3422ab6fa969100b2f433a57c9a1cf1b",
+    "url": "/static/media/me.3422ab6f.png"
+  },
+  {
+    "revision": "67081e4db18ad39a6aff1c4cf86fdd7a",
+    "url": "/static/media/mongo.67081e4d.png"
+  },
+  {
+    "revision": "b337a2fb0b1289e6757e632dc766ff6b",
+    "url": "/static/media/nodejs.b337a2fb.svg"
+  },
+  {
+    "revision": "8845650f314eeddb8315b26a012620db",
+    "url": "/static/media/petgram.8845650f.png"
+  },
+  {
+    "revision": "4f557e16a1f8f9382602bb1b8def44cc",
+    "url": "/static/media/pickle.4f557e16.png"
+  },
+  {
+    "revision": "687f11cb75d0c857ad0e2ecff2df9a4f",
+    "url": "/static/media/styledComponents.687f11cb.png"
+  },
+  {
+    "revision": "09b69d3582e021b50157e4db106ddc80",
+    "url": "/static/media/swift.09b69d35.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cecam92.github.io/precache-manifest.34c5a216a41eb45c837775ef3b8f3046.js"
+  "/precache-manifest.3d5b07dfac9e34665c104500f42d4db8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cecam92.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
