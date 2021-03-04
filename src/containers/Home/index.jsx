386591be0f11e866a5150bbac7f22a13d1f351a2
Cesar Martinez <cecam92@hotmail.com>
2021-03-04 18:50:38 +1100

    profile picture updated

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,43 +1,44 @@
-import React, { Fragment, useState, useEffect } from "react";
-import Header from "../../components/Header";
-import Nav from "../../components/Navigation";
-import Main from "../../components/Main";
-import Footer from "../../components/Footer";
-import { useSpring, animated as a } from "react-spring";
+import React, { Fragment, useState, useEffect } from 'react'
+import Header from '../../components/Header'
+import Nav from '../../components/Navigation'
+import Main from '../../components/Main'
+import Footer from '../../components/Footer'
+import { useSpring, animated as a } from 'react-spring'
 
 const Home = () => {
-  const [data, setData] = useState(1);
-  const [stick, setStick] = useState(false);
+  const [data, setData] = useState(1)
+  const [stick, setStick] = useState(false)
 
   const fadeProps = useSpring({
     opacity: stick ? 1 : 0,
-    backgroundColor: stick ? "#22262b" : "#22262b",
-  });
+    backgroundColor: stick ? '#22262b' : '#22262b'
+  })
 
   function cutNumber(num) {
-    const MIN = 1;
-    const MAX = 96;
-    const parsed = parseInt(num);
-    return Math.min(Math.max(parsed, MIN), MAX);
+    const MIN = 1
+    const MAX = 96
+    const parsed = parseInt(num)
+    return Math.min(Math.max(parsed, MIN), MAX)
   }
   function scrolling() {
-    setData(cutNumber(window.scrollY));
+    setData(cutNumber(window.scrollY))
   }
   function getStick() {
     if (data >= 96) {
-      setStick(true);
+      setStick(true)
     } else {
-      setStick(false);
+      setStick(false)
     }
   }
   useEffect(() => {
-    window.addEventListener("scroll", scrolling);
-    getStick();
+    window.addEventListener('scroll', scrolling)
+    getStick()
 
     return () => {
-      window.removeEventListener("scroll", scrolling);
-    };
-  }, [data]);
+      window.removeEventListener('scroll', scrolling)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data])
 
   return (
     <Fragment>
@@ -52,11 +53,11 @@ const Home = () => {
       <a.div style={fadeProps}>
         <>
           <Main />
-          <Footer className="fixed-bottom" />
+          <Footer className='fixed-bottom' />
         </>
       </a.div>
     </Fragment>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
